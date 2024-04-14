4893821edcb767a5a5050551ba8b5b12d98728ba
= <rishitpallav9@gmail.com>
2024-04-14 15:50:10 -0500

    Spring Boot Created

---
+++ shareameal/src/main/java/com/iit/shareameal/ShareamealApplication.java
@@ -0,0 +1,13 @@
+package com.iit.shareameal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareamealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareamealApplication.class, args);
+	}
+
+}
---
+++ shareameal/src/test/java/com/iit/shareameal/ShareamealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.shareameal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareamealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
